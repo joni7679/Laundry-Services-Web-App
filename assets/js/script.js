522f46f76e591ea12e1b70ec14c22d536493e7dd
joni <codingmylife0@gmail.com>
2025-09-05 14:47:07 +0530

    task complete....

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,26 +8,21 @@ togglebtn.addEventListener('click', () => {
 let itemList = document.querySelector(".item-list");
 let totalPrice = document.querySelector("#total-price");
 let cardList = document.querySelector(".card-list")
-let form = document.querySelector("form");
-let userName = document.querySelector("#username");
-let userEmail = document.querySelector("#email")
-let userPhoneNumber = document.querySelector("#number")
 
 let cart = [];
 
 const services = [
-    { name: "dry cleaning", price: 200, icon: `<i class="ri-t-shirt-air-line"></i>` },
-    { name: "wash & fold ", price: 100, icon: `<i class="ri-t-shirt-air-line"></i>` },
-    { name: "ironing", price: 300, icon: `<i class="ri-t-shirt-air-line"></i>` },
-    { name: "stain removal", price: 500, icon: `<i class="ri-t-shirt-air-line"></i>` },
-    { name: "leather & suede cleaning", price: 999, icon: `<i class="ri-t-shirt-air-line"></i>` },
-    { name: "wedding dress cleaning", price: 2800, icon: `<i class="ri-t-shirt-air-line"></i>` }
+    { name: "dry cleaning", price: 200, },
+    { name: "wash & fold ", price: 100, },
+    { name: "ironing", price: 300, },
+    { name: "stain removal", price: 500, },
+    { name: "leather & suede cleaning", price: 999, },
+    { name: "wedding dress cleaning", price: 2800, }
 ];
 let clutter = '';
 services.forEach((item, index) => {
     clutter += `<div class="service">
                             <div class="d-flex">
-                                <p> ${item.icon} </p>
                                 <p> ${item.name} </p>
                                 <p>₹${item.price}</p>
                             </div>
@@ -43,7 +38,7 @@ function addToCart(index) {
     if (!isAddBuuton) {
         // push data my add cart 
         cart.push(services[index]);
-       console.log("after cart push plz check it",cart);
+        console.log("after cart push plz check it", cart);
         clickButton.dataset.added = "true";
         clickButton.style.backgroundColor = "#fab1a4ff";
         clickButton.style.color = "#ec3d1eff";
@@ -58,21 +53,17 @@ function addToCart(index) {
             clickButton.style.color = "var(--text-bg-color)",
             clickButton.innerHTML = `Add Item <i class="ri-shopping-cart-2-line"></i>`;
         isAddBuuton = false;
-
     }
     updateCart();
 }
 
-function removeCart(item) {
-
-    updateCart();
-};
 let noCartItems = document.querySelector(".no-cart-items")
 function updateCart() {
     let clutter = '';
     let total = 0;
     if (cart.length === 0) {
         noCartItems.style.display = "block";
+        noCartItems.style.display = "flex"
     }
     else if (cart.length > 0) {
         noCartItems.style.display = "none";
@@ -91,11 +82,63 @@ function updateCart() {
     cardList.innerHTML = clutter;
 };
 
+let form = document.querySelector("form");
+let userName = document.querySelector("#username");
+let userEmail = document.querySelector("#email");
+let userPhoneNumber = document.querySelector("#number");
+let loadingSms = document.querySelector("#loading-sms");
+let bookSms = document.querySelector("#book-sms");
+let errorProductSms = document.querySelector("#error-product")
 // submit form logic here
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let userNameValue = userName.value;
-    let userEmilValue = userEmail.value;
-    let numberValue = userPhoneNumber.value;
-    console.log(userNameValue, userEmilValue, numberValue);
+    let userNameValue = userName.value.trim();
+    let userEmailValue = userEmail.value.trim();
+    let numberValue = userPhoneNumber.value.trim();
+    let userNameEroorSms = document.querySelector('#user-error-sms');
+    let userEmailErrorSms = document.querySelector("#email-error-sms");
+    let userPhoneNumberError = document.querySelector("#number-error-sms")
+    let isValid = true;
+
+    // user name validation...
+    if (!userNameValue) {
+        userNameEroorSms.textContent = "please enter your user name... ";
+        isValid = false;
+    }
+    else {
+        userNameEroorSms.textContent = "";
+    }
+    // user email validation...
+    if (!userEmailValue) {
+        userEmailErrorSms.textContent = "please enter your emil id here... ";
+        isValid = false;
+    }
+    else {
+        userEmailErrorSms.textContent = "";
+    }
+    // user number  validation...
+    if (!numberValue) {
+        userPhoneNumberError.textContent = "please enter your phone number here...";
+        isValid = false;
+    }
+    else {
+        userPhoneNumberError.textContent = "";
+    }
+    if (cart.length === 0) {
+        errorProductSms.textContent = "plase at least one product add..."
+        isValid = false;
+    }
+    else if (cart.length > 0) {
+        errorProductSms.textContent = "";
+        if (isValid) {
+             loadingSms.style.display = "block";
+            console.log("isValid", isValid);
+            emailjs.sendForm("service_ooedkzt", "template_d4x447i", form, "ExPnBFMVksHWTsatH").then(() => {
+                loadingSms.style.display = "none";
+                bookSms.textContent = "Emil has send successfully";
+            }).catch((error) => {
+                console.log(error, "somthing is wrong plz try agin later...");
+            })
+        }
+    }
 })
